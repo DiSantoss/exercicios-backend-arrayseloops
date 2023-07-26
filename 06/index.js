3546522afdad2c1ca010552627296b46428ff46e
Di <d.2120leandro@gmail.com>
2023-07-26 19:33:08 -0300

    sextoex

--- 06/index.js
+++ 06/index.js
@@ -1,2 +1,10 @@
 const numeros = [3, 4, 7, 8, 1, 6, 5, 10];
+let soma = 0;
 
+for(let resultado of numeros) {
+    if (resultado % 2 === 0) {
+        soma = soma + resultado
+    }
+}
+
+console.log(soma)
