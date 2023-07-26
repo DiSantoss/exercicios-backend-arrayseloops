e8ec37513a3c44be469c734b3e7d5dae386d8f1b
Di <d.2120leandro@gmail.com>
2023-07-26 19:40:04 -0300

    setimoex

--- 07/index.js
+++ 07/index.js
@@ -1,1 +1,11 @@
 const nomes = ["Ana", "Joana", "Carlos", "amanda"];
+const nomesComA = [];
+
+for (const resultado of nomes) {
+  const nomeLowerCase = resultado.toLowerCase();
+  if (nomeLowerCase.startsWith("a") || resultado.startsWith("A")) {
+    nomesComA.push(resultado);
+  }
+}
+
+console.log(nomesComA);
