5d1cb23e8e290d68b71589e423043608740b063b
Di <d.2120leandro@gmail.com>
2023-07-26 19:22:53 -0300

    terceiroex

--- 03/index.js
+++ 03/index.js
@@ -1,2 +1,9 @@
 const numeros = [54, 22, 14, 87, 10, 284];
 
+for(let resultado of numeros) {
+    if(resultado == 10) {
+        console.log(numeros.indexOf(10))
+    } else {
+        console.log("-1")
+    }
+}
