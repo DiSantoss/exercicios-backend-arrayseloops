8895c0d5d319e1374a63df298f226a4b7ea10eee
Di <d.2120leandro@gmail.com>
2023-07-26 19:30:37 -0300

    quintoex

--- 05/index.js
+++ 05/index.js
@@ -1,1 +1,11 @@
 const original = [1, 4, 12, 21, 53, 88, 112];
+const resultadooriginal = [];
+
+for(const resultado of original) {
+    if(resultado % 2 === 0) {
+        resultadooriginal.push(resultado)
+       
+    }
+  
+}
+console.log(resultadooriginal)
