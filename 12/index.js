f2e200fa1503b388bb238ddf8413b4ee22e82290
Di <d.2120leandro@gmail.com>
2023-07-26 23:54:45 -0300

    dsegundocommit

--- 12/index.js
+++ 12/index.js
@@ -1,3 +1,12 @@
 const filaDeDentro = ["Jose", "Maria", "Joao"];
 const filaDeFora = ["Joana", "Roberta", "Marcos", "Felipe"];
+const maximoPessoasDentro = 4
 
+for(let resultado of filaDeFora) {
+    if(filaDeDentro.length <= maximoPessoasDentro) {
+        filaDeDentro.push(resultado)
+    }
+}
+
+
+ console.log(filaDeDentro)
