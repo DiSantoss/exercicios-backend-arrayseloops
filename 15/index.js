7ea4245a56165cf0cc79c369d235756450b28d45
Di <d.2120leandro@gmail.com>
2023-07-27 00:30:52 -0300

    dquintocommit

--- 15/index.js
+++ 15/index.js
@@ -2,3 +2,16 @@ const original = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const pares = [];
 const impares = [];
 
+for (let resultado of original) {
+    if(resultado % 2 === 0) {
+        pares.push(resultado)
+    } else {
+        impares.push(resultado)
+    }
+}
+
+console.log(pares)
+console.log(impares)
+
+
+
