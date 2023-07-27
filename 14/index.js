3ee8882170157dbe2dea23eec24abf44fdb63f96
Di <d.2120leandro@gmail.com>
2023-07-27 00:27:53 -0300

    decimoquartocommit

--- 14/index.js
+++ 14/index.js
@@ -1,3 +1,8 @@
 const arrayA = [5, 32, 3, 44, 1];
 const arrayB = [57, 4, 21, 2, 13];
 
+for (const [indice, valorA] of arrayA.entries()) {
+  const valorB = arrayB[indice];
+  const menorValor = valorA < valorB ? valorA : valorB;
+  console.log(menorValor);
+}
