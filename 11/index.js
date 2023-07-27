67dc3e8b9e10c619d1025f42b694d99b5a5dad7f
Di <d.2120leandro@gmail.com>
2023-07-26 23:40:30 -0300

    dprimeiroex

--- 11/index.js
+++ 11/index.js
@@ -1,2 +1,12 @@
 const original = [5, 7, 13, 17, 26, 34, 118, 245];
+let novoArray = []
+for (let resultado of original) {
+    if(resultado > 10 && resultado < 20) {
+        novoArray.push(resultado)
+    } else if (resultado > 100) {
+        novoArray.push(resultado)
+    }
+ 
+}
 
+console.log(novoArray)
