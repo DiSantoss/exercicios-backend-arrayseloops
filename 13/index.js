5de9d4839f911f3592e7968609f47ec70b6a9649
Di <d.2120leandro@gmail.com>
2023-07-27 00:22:05 -0300

    dterceirocommit

--- 13/index.js
+++ 13/index.js
@@ -1,2 +1,20 @@
-const disjuntores = [false, false, true, false, false, true, false, false];
+const disjuntores = [
+    false,
+    true,
+    true,
+    false,
+    false,
+    true,
+    false,
+    false,
+    true,
+    true,
+  ];
+let totaldisjuntores = []
 
+for(const [indice, valor] of disjuntores.entries()){
+    if(valor === true) {
+        totaldisjuntores.push(indice)
+    }
+}
+    console.log(totaldisjuntores)
